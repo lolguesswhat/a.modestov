b3783c97a2233b6814455d78cacbe948fa641f82
Alexander Modestov <modestov.1998@gmail.com>
2022-06-08 17:30:52 +0400

    Task corrected

--- src/test/java/tests/RestApiTest.java
+++ src/test/java/tests/RestApiTest.java
@@ -4,7 +4,6 @@ import dao.Post;
 
 import dao.User;
 import feign.Response;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,8 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.testng.asserts.SoftAssert;
 import utils.propertiesReader.ResponseBodyDecoder;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -28,43 +25,43 @@ import static utils.propertiesReader.ReaderGetter.*;
 @SpringBootApplication
 @EnableFeignClients
 public class RestApiTest extends BaseTest {
+    private SoftAssert softAssert;
 
     @Test
     public void jsonPlaceholderTest() {
-
         Logger logger = LoggerFactory.getLogger("Test steps");
         logger.debug("Step 1");
-        SoftAssert step1Assert = new SoftAssert();
+        softAssert = new SoftAssert();
         Response postsResponse = jsonPlaceholder.getListOfPostsResponse();
-        step1Assert.assertEquals(postsResponse.status(), HttpStatus.OK.value(), "Response status does not match");
-        step1Assert.assertTrue(isBodyJson(postsResponse.body().toString()), "Response body is not json");
+        softAssert.assertEquals(postsResponse.status(), HttpStatus.OK.value(), "Response status does not match");
+        softAssert.assertTrue(isBodyJson(postsResponse.body().toString()), "Response body is not json");
         List<Post> posts = getListOfPojo(postsResponse, Post[].class);
-        IntStream.range(0, posts.size() - 1).forEach(x -> step1Assert.assertTrue(posts.get(x).getId() < posts.get(x + 1).getId(),
+        IntStream.range(0, posts.size() - 1).forEach(x -> softAssert.assertTrue(posts.get(x).getId() < posts.get(x + 1).getId(),
                 "Posts are not sorted ascending"));
-        step1Assert.assertAll();
+        softAssert.assertAll();
 
         logger.debug("Step 2");
-        SoftAssert step2Assert = new SoftAssert();
+        softAssert = new SoftAssert();
         Response postResponse = jsonPlaceholder.getPostByIdResponse(getTestDataReader().test2PostNumber());
-        step2Assert.assertEquals(postResponse.status(), HttpStatus.OK.value(), "Response status does not match");
+        softAssert.assertEquals(postResponse.status(), HttpStatus.OK.value(), "Response status does not match");
         Post postToGetStep2 = getPojo(jsonPlaceholder.getPostByIdResponse(getTestDataReader().test2PostNumber()), Post.class);
-        step2Assert.assertEquals(parseInt(postToGetStep2.getUserId().toString()), getTestDataReader().test2ExpectedUserId(), "User id does not match");
-        step2Assert.assertEquals(parseInt(postToGetStep2.getId().toString()), getTestDataReader().test2PostNumber(), "Post id does not match");
-        step2Assert.assertNotNull(postToGetStep2.getBody(), "Body is null");
-        step2Assert.assertNotNull(postToGetStep2.getTitle(), "Title is null");
-        step2Assert.assertAll();
+        softAssert.assertEquals(parseInt(postToGetStep2.getUserId().toString()), getTestDataReader().test2ExpectedUserId(), "User id does not match");
+        softAssert.assertEquals(parseInt(postToGetStep2.getId().toString()), getTestDataReader().test2PostNumber(), "Post id does not match");
+        softAssert.assertNotNull(postToGetStep2.getBody(), "Body is null");
+        softAssert.assertNotNull(postToGetStep2.getTitle(), "Title is null");
+        softAssert.assertAll();
 
         logger.debug("Step 3");
-        SoftAssert step3Assert = new SoftAssert();
+        softAssert = new SoftAssert();
         Response postToGetStep3 = jsonPlaceholder.getPostByIdResponse(getTestDataReader().test3PostNumber());
-        step3Assert.assertEquals(postToGetStep3.status(), HttpStatus.NOT_FOUND.value(), "Status code does not match");
-        step3Assert.assertEquals(ResponseBodyDecoder.decode(postToGetStep3),
+        softAssert.assertEquals(postToGetStep3.status(), HttpStatus.NOT_FOUND.value(), "Status code does not match");
+        softAssert.assertEquals(ResponseBodyDecoder.decode(postToGetStep3),
                 getTestDataReader().test3ExpectedBody(),
                 "Body is not empty");
-        step3Assert.assertAll();
+        softAssert.assertAll();
 
         logger.debug("Step 4");
-        SoftAssert step4Assert = new SoftAssert();
+        softAssert = new SoftAssert();
         Post postToSend = Post
                 .builder()
                 .body(randomAlphabetic((int) (random() * 10)))
@@ -72,29 +69,29 @@ public class RestApiTest extends BaseTest {
                 .userId(getTestDataReader().test4UserId())
                 .build();
         Post responsePost = getPojo(jsonPlaceholder.sendPost(postToSend), Post.class);
-        step4Assert.assertEquals(responsePost.getTitle(), postToSend.getTitle(), "Sent Title does not match response");
-        step4Assert.assertEquals(responsePost.getBody(), postToSend.getBody(), "Sent Body does not match response");
-        step4Assert.assertEquals(responsePost.getUserId(), postToSend.getUserId(), "Sent UserId does not match response");
-        step4Assert.assertNotNull(responsePost.getId(), "Response does not contain variable 'id'");
-        step4Assert.assertAll();
+        softAssert.assertEquals(responsePost.getTitle(), postToSend.getTitle(), "Sent Title does not match response");
+        softAssert.assertEquals(responsePost.getBody(), postToSend.getBody(), "Sent Body does not match response");
+        softAssert.assertEquals(responsePost.getUserId(), postToSend.getUserId(), "Sent UserId does not match response");
+        softAssert.assertNotNull(responsePost.getId(), "Response does not contain variable 'id'");
+        softAssert.assertAll();
 
         logger.debug("Step 5");
-        SoftAssert step5Assert = new SoftAssert();
+        softAssert = new SoftAssert();
         Response listOfUsersResponse = jsonPlaceholder.getListOfUsersResponse();
-        step5Assert.assertEquals(listOfUsersResponse.status(), HttpStatus.OK.value(), "Response status does not match");
-        step5Assert.assertTrue(isBodyJson(listOfUsersResponse.body().toString()), "Response body is not json");
+        softAssert.assertEquals(listOfUsersResponse.status(), HttpStatus.OK.value(), "Response status does not match");
+        softAssert.assertTrue(isBodyJson(listOfUsersResponse.body().toString()), "Response body is not json");
         User userToCheck = getListOfPojo(jsonPlaceholder.getListOfUsersResponse(), User[].class).get(getTestDataReader().test5ExpectedId() - 1);
         User userToCheckWithFromList = getPojoFromFile(getTestDataReader().test5PathToUser5(), User.class);
-        step5Assert.assertEquals(userToCheck, userToCheckWithFromList, "User 5's data does not match test data");
-        step5Assert.assertAll();
+        softAssert.assertEquals(userToCheck, userToCheckWithFromList, "User 5's data does not match test data");
+        softAssert.assertAll();
 
         logger.debug("Step 6");
-        SoftAssert step6Assert = new SoftAssert();
+        softAssert = new SoftAssert();
         Response userToCheckWithResponse = jsonPlaceholder.getUserByIdResponse(getTestDataReader().test5ExpectedId());
-        step6Assert.assertEquals(userToCheckWithResponse.status(), HttpStatus.OK.value(), "Response status does not match");
-        step6Assert.assertTrue(isBodyJson(userToCheckWithResponse.body().toString()), "Response body is not json");
+        softAssert.assertEquals(userToCheckWithResponse.status(), HttpStatus.OK.value(), "Response status does not match");
+        softAssert.assertTrue(isBodyJson(userToCheckWithResponse.body().toString()), "Response body is not json");
         User userToCheckWith = getPojo(jsonPlaceholder.getUserByIdResponse(getTestDataReader().test5ExpectedId()), User.class);
-        step6Assert.assertEquals(userToCheck, userToCheckWith, "User 5's data does not match test data");
-        step6Assert.assertAll();
+        softAssert.assertEquals(userToCheck, userToCheckWith, "User 5's data does not match test data");
+        softAssert.assertAll();
     }
 }
